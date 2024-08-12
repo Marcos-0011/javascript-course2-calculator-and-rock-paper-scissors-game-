7c07a00cc9a35cf08bf4d5c3188f2c9af3d0b82f
Marcos Villarreal <marcosvilla444@gmail.com>
2024-08-12 14:35:03 -0500

    Lesson 12: Advanced Functions 2 Video Done

--- Lesson12/scripts/12.1.2-rock-paper-scissors.js
+++ Lesson12/scripts/12.1.2-rock-paper-scissors.js
@@ -9,8 +9,9 @@ updateScoreElement();
 let isAutoPlaying = false;
 let intervalID;
 
+// 12.4.1
 //const autoPlay = () => {
-
+// using an arrow function for the first function is personal preference but a regular function is easier to read and allows hoisting
 //};
 function autoPlay () {
   if (!isAutoPlaying) {
@@ -28,6 +29,30 @@ function autoPlay () {
     isAutoPlaying = false;
   }
 }
+
+// 12.4.1: a common mistake here is to just do playGame('rock') for the second value. however, that will actually run the function and return undefined and that will be given to addEventListener. instead we need to give it a function, so we'll do an arrow function and inside will be playGame('rock')
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+// 12.4.1 advanced functions 2 part 2: when we click a r, p, or s anywhere on the page, it will play the game. have to do body bc it will anywhere on the page. running addEventListener saves the event object and we use it as the parameter function in the second value. the event object has the key property that records what key was pressed. now go to 12.1.3 todolist files to use addEventListener
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
--- Lesson12/scripts/12.1.3-todo-list.js
+++ Lesson12/scripts/12.1.3-todo-list.js
@@ -11,15 +11,12 @@ renderTodoList();
 function renderTodoList () {
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, date } = todoObject;
     const html = `
       <div>${name}</div> 
       <div>${date}</div>
-      <button class="delete-todo-button" onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-      ">Delete</button>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
     `;
 
     todoListHTML += html;
@@ -27,8 +24,20 @@ function renderTodoList () {
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
+  // 12.4.1 advanced functions 2 part 2: we have to add addEventListeners to the delete buttons after they're put on the page. we also have multiple delete buttons so we can use querySelectorAll to select all the delete buttons. console.logging document.querySelectorAll('.js-delete-todo-button); shows an array of the delete buttons. so we can loop through them and add addEventListeners. now go back to 12.4.1 for advanced functions 2 part 3
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(index, 1);
+      renderTodoList();
+    });
+  });
+
   localStorage.setItem('todoList', JSON.stringify(todoList));
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+})
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
